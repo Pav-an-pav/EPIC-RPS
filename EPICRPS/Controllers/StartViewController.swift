2a418c308479c920196eecb283029a204cf82a74
Ivan Semenov <izzuyinvest@gmail.com>
2024-06-10 14:41:27 +0300

    add project

---
+++ EPICRPS/Controllers/StartViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StartViewController.swift
+//  EPICRPS
+//
+//  Created by Xcode on 10.06.2024.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+}
+
